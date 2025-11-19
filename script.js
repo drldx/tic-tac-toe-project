e781519e17623baecb9c4fea496b397e8b1b499d
drldx <168914904+drldx@users.noreply.github.com>
2025-11-19 13:53:58 +0530

    add: GameBoard and Cell modules
    function GameBoard and Cell were added
    for methods getBoard, drawToken, printBoard
    and addToken with getValue

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("hello");
+function GameBoard(){
+    const board = [];
+    //2d array
+    for(let i = 0; i < 3; i++){
+        board[i] = [];
+        for(let j = 0; j < 3; j++){
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = ()=> board;
+
+    const drawToken = (player, row, col) => {
+        if(board[row][col]) return;
+
+        board[row][col].addToken(player);
+    }
+
+    const printBoard = ()=> {
+        const boardWithCellValues = board.map(row => row.map(cell => cell.getValue()));
+        console.log(boardWithCellValues);
+    }
+    return {getBoard,
+            drawToken,
+            printBoard};
+}
+
+function Cell(){
+    let value = "";
+    const addToken = (player) => {
+        value = player;
+    }
+    const getValue = () => value; 
+        
+    return {getValue, addToken};
+}
